4e5b9695c3fd54fd60c47cc0d39b16a658149918
Gerald-Star <ukwuomagerald@gmail.com>
2024-09-07 18:53:44 +0200

    create fetchPet component  to provide context using React Query

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+// Wrap the entire Browser application in QueryClientProvider
 
 const App = () => {
   return (
     // replace <SearchParams /> and <h1>Adopt Me!</h1>
     <BrowserRouter>
-      <h1>Adopt Me!</h1>
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        {/*<h1>Adopt Me!</h1>*/}
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
